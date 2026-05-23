d21611d7ff1180a124b1516e04351fa90255cd72
Stephen Workplace <stephenworkplace@macbookpro.lan>
2026-05-22 19:10:36 -0700

    Implement saved list with remove + add-to-itinerary actions

--- src/components/SavedTab.tsx
+++ src/components/SavedTab.tsx
@@ -1,5 +1,6 @@
 import type { Place } from '../types';
+import { SavedList } from './SavedList';
 
-export function SavedTab(_: { onAddToItinerary: (place: Place) => void }) {
-  return <div>Saved (coming up)</div>;
+export function SavedTab({ onAddToItinerary }: { onAddToItinerary: (place: Place) => void }) {
+  return <SavedList onAddToItinerary={onAddToItinerary} />;
 }
